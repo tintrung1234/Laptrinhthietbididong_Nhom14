efee23d2d55f497f8620ecdfa99dbb6e7ce8a096
Zung22H <kaibahp1@gmail.com>
2025-04-15 03:03:45 +0700

    them man hinh doanh thu vao

--- OwnerScreen/app/build.gradle.kts
+++ OwnerScreen/app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
     implementation ("com.google.accompanist:accompanist-insets:0.30.1")
+    implementation ("androidx.navigation:navigation-compose:2.7.6")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- OwnerScreen/app/src/main/java/com/example/ownerscreen/MainActivity.kt
+++ OwnerScreen/app/src/main/java/com/example/ownerscreen/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -37,6 +38,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Notifications
@@ -46,10 +48,25 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import java.time.format.TextStyle
 
 
@@ -58,14 +75,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HomeScreen()
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "home") {
+                composable("home") { HomeScreen(navController) }
+                composable("revenue") { RevenueScreen(navController) }
+            }
         }
+
+
     }
 }
 
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavHostController) {
     Box(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
@@ -99,7 +122,7 @@ fun HomeScreen() {
             modifier = Modifier
                 .align(Alignment.BottomCenter)
         ) {
-            BottomNavigationBar()
+            BottomNavigationBar(navController)
         }
     }
 }
@@ -372,7 +395,7 @@ fun StaffInfoSection() {
 }
 
 @Composable
-fun BottomNavigationBar() {
+fun BottomNavigationBar(navController: NavHostController) {
     BottomAppBar(
         modifier = Modifier.fillMaxWidth(),
         containerColor = Color(0xFFEADFA2), // màu nền bar
@@ -390,6 +413,11 @@ fun BottomNavigationBar() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .weight(1f)
+                    .clickable {
+                        if (label == "Doanh thu") {
+                            navController.navigate("revenue")
+                        }
+                    }
                     .padding(vertical = 8.dp)
             ) {
                 when (icon) {
@@ -415,11 +443,165 @@ fun BottomNavigationBar() {
 }
 
 
+// man hinh doanh thu
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RevenueScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Doanh thu",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                },
+                actions = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.logo_2),
+                        contentDescription = null,
+                        tint = Color.Unspecified,
+                        modifier = Modifier
+                            .padding(end = 4.dp)
+                            .size(70.dp)
+                    )
+                }
+            )
+        }
+    ) { innerPadding ->
+
+        // Bọc nội dung trong Column + VerticalScroll để tránh tràn
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+
+
+            // Nút chọn ngày
+            Button(
+                onClick = { /* TODO: mở dialog chọn ngày */ },
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    containerColor = Color.White,
+                    contentColor = Color.Black
+                ),
+                border = BorderStroke(1.dp, Color.LightGray),
+                modifier = Modifier.wrapContentWidth()
+
+            ) {
+                Icon(Icons.Default.DateRange, contentDescription = null)
+                Spacer(Modifier.width(4.dp))
+                Text("Hôm nay")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                RevenueCard(
+                    title = "TỔNG DOANH THU",
+                    value = "50.000.000",
+                    color = Color.Red,
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                RevenueCard(
+                    title = "Số lượng đơn hoàn thành",
+                    value = "15",
+                    color = Color.Black,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            RevenueCard(
+                title = "Dịch vụ bán chạy nhất",
+                value = "Massage",
+                color = Color.Red,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Text(
+                text = "Doanh thu hôm nay",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.chart_doanhthu),
+                contentDescription = "Chart",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(450.dp), // tránh để auto quá cao
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+
+
+@Composable
+fun RevenueCard(
+    title: String,
+    value: String,
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        border = BorderStroke(1.dp, Color.DarkGray),
+        colors = CardDefaults.cardColors(containerColor = Color.White) // màu be nhạt
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = value,
+                color = color,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
 
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    HomeScreen()
+    val navController = rememberNavController()
+    RevenueScreen(navController)
 }
